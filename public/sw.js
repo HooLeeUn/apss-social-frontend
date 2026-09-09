903a16d370975164c0dfb0f8e51277cb8fef68ad
Julian Hernandez <julhernaferna@gmail.com>
2026-09-08 19:22:02 -0500

    Refine ReCCool PWA icon background and alignment

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,5 @@
 const CACHE_PREFIX = "reccool-static-";
-const STATIC_CACHE = `${CACHE_PREFIX}v2`;
+const STATIC_CACHE = `${CACHE_PREFIX}v3`;
 const SAFE_STATIC_PATHS = new Set([
   "/manifest.webmanifest",
   "/icons/pwa/icon-192.png",
